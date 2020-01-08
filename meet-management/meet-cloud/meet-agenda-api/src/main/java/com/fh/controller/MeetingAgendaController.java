44772298263890cbfb296ad8ae6ca0365e9578f6
xiaotian991001 <379192173@qq.com>
2020-01-08 21:06:44 +0800

    ztree主页面

---
+++ meet-management/meet-cloud/meet-agenda-api/src/main/java/com/fh/controller/MeetingAgendaController.java
@@ -0,0 +1,14 @@
+package com.fh.controller;
+
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("meetingAgenda")
+@CrossOrigin
+public class MeetingAgendaController {
+
+
+}
