dfc445b0d390afd8099f99164a814d9a7a7bb1ad
bsy <3014857362@qq.com>
2020-01-08 19:08:13 +0800

    白少玉 room 会议室介绍 完成查询

--- meet-management/meet-cloud/meet-room-reserve-api/src/main/java/com/fh/mapper/IRoomMapper.java
+++ meet-management/meet-cloud/meet-room-reserve-api/src/main/java/com/fh/mapper/IRoomMapper.java
@@ -1,4 +1,18 @@
 package com.fh.mapper;
 
+import com.fh.beans.Room;
+import com.fh.utils.PageBean;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Mapper
+@Repository
 public interface IRoomMapper {
+ Long getCount();
+
+ List<Room> queryRoom(@Param("page") PageBean<Room> page);
+
 }
--- meet-management/meet-cloud/meet-room-reserve-api/src/main/java/com/fh/service/IRoomService.java
+++ meet-management/meet-cloud/meet-room-reserve-api/src/main/java/com/fh/service/IRoomService.java
@@ -1,4 +1,9 @@
 package com.fh.service;
 
+import com.fh.beans.Room;
+import com.fh.utils.PageBean;
+
 public interface IRoomService {
+
+ PageBean<Room> queryRoom(PageBean<Room> page);
 }
