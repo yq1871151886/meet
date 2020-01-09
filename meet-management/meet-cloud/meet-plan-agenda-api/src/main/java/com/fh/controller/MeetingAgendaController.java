914e1e218ec760fef2ac20551178d0986012500e
xiaotian991001 <379192173@qq.com>
2020-01-09 23:31:19 +0800

    项目中引用swagger

--- meet-management/meet-cloud/meet-plan-agenda-api/src/main/java/com/fh/controller/MeetingAgendaController.java
+++ meet-management/meet-cloud/meet-plan-agenda-api/src/main/java/com/fh/controller/MeetingAgendaController.java
@@ -5,9 +5,14 @@ import com.fh.bean.MeetingAgendaBean;
 import com.fh.enumbean.ResponseServer;
 import com.fh.service.IMeetingAgendaService;
 import com.fh.utils.PageBean;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+
+
+@Api(value = "议题")
 @RestController
 @RequestMapping("meetingAgenda")
 @CrossOrigin(value = "http://localhost:8080",maxAge = 3600)
@@ -20,6 +25,7 @@ public class MeetingAgendaController {
      * 查询下拉里面的属性
      * @return
      */
+    @ApiOperation(value = "获取所有的议题列表")
     @GetMapping
     public ResponseServer initMeeting(){
 
@@ -32,6 +38,7 @@ public class MeetingAgendaController {
      * @return
      *
      */
+    @ApiOperation(value = "新增一个议题")
     @PostMapping("addMeeting")
     public ResponseServer addMeeting(MeetingAgendaBean meetingAgenda){
         meetingAgendaService.addMeeting(meetingAgenda);
@@ -39,6 +46,7 @@ public class MeetingAgendaController {
         return ResponseServer.success();
     }
 
+    @ApiOperation(value = "审批议题")
     @PostMapping("meetingPage")
     public PageBean<MeetingAgendaBean> meetingPage(PageBean<MeetingAgendaBean> page,MeetingAgendaBean meetingAgenda){
 
--- meet-management/meet-cloud/meet-plan-agenda-api/src/test/java/com/fh/MeetPlanAgendaApiApplicationTests.java
+++ meet-management/meet-cloud/meet-plan-agenda-api/src/test/java/com/fh/MeetPlanAgendaApiApplicationTests.java
@@ -1,13 +1,13 @@
 package com.fh;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class MeetPlanAgendaApiApplicationTests {
+public class MeetPlanAgendaApiApplicationTests {
 
     @Test
-    void contextLoads() {
+    public void contextLoads() {
     }
 
 }
